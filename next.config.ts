a040f3d0524e9601f2447080df80dbdbe4326fde
KevinCarpricorn <zhengyuxiang8888@gmail.com>
2025-11-13 00:40:03 +1100

    chore: enable static export and github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-images: {
+  output: "export",
+  images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: "images.unsplash.com",
