b44f0dff34b3fb2a5e2cda583bd9fb7e9f2aa4ca
KevinCarpricorn <zhengyuxiang8888@gmail.com>
2025-11-14 02:22:44 +1100

    Make Orcid icon theme-aware

--- src/components/icons/AcademicIcons.tsx
+++ src/components/icons/AcademicIcons.tsx
@@ -28,11 +28,10 @@ export function GoogleScholarWordmarkIcon(props: SVGProps<SVGSVGElement>) {
 
 export function OrcidIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" {...props}>
-      <circle cx="12" cy="12" r="10" fill="#A6CE39" />
+    <svg viewBox="0 0 32 32" aria-hidden="true" focusable="false" {...props}>
       <path
-        fill="#fff"
-        d="M9.2 7.5v9h-1.7v-9h1.7zm3.5-.1c2.4 0 4.3 1.9 4.3 4.5 0 2.7-1.9 4.7-4.3 4.7s-4.3-2-4.3-4.7c0-2.6 1.9-4.5 4.3-4.5zm0 1.6c-1.5 0-2.6 1.3-2.6 2.9 0 1.7 1.1 3 2.6 3s2.6-1.3 2.6-3c0-1.6-1.1-2.9-2.6-2.9z"
+        d="M16 0c-8.839 0-16 7.161-16 16s7.161 16 16 16c8.839 0 16-7.161 16-16S24.839 0 16 0ZM9.823 5.839c.704 0 1.265.573 1.265 1.26 0 .688-.561 1.265-1.265 1.265-.692-.004-1.26-.567-1.26-1.265 0-.697.563-1.26 1.26-1.26ZM8.864 9.885h1.923v13.391H8.864Zm4.751 0h5.197c4.948 0 7.125 3.541 7.125 6.703 0 3.439-2.687 6.699-7.099 6.699h-5.224Zm1.921 1.74v9.927h3.063c4.365 0 5.365-3.312 5.365-4.964 0-2.687-1.713-4.963-5.464-4.963Z"
+        fill="currentColor"
       />
     </svg>
   );
