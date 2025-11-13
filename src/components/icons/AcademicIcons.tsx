0a6de694cceafa6edf7f0bb9454422dfeec6ec21
KevinCarpricorn <zhengyuxiang8888@gmail.com>
2025-11-14 02:39:23 +1100

    Use colored ORCID icon in contact modal

--- src/components/icons/AcademicIcons.tsx
+++ src/components/icons/AcademicIcons.tsx
@@ -36,3 +36,15 @@ export function OrcidIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function OrcidIconColor(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" {...props}>
+      <circle cx="12" cy="12" r="10" fill="#A6CE39" />
+      <path
+        fill="#fff"
+        d="M9.2 7.5v9h-1.7v-9h1.7zm3.5-.1c2.4 0 4.3 1.9 4.3 4.5 0 2.7-1.9 4.7-4.3 4.7s-4.3-2-4.3-4.7c0-2.6 1.9-4.5 4.3-4.5zm0 1.6c-1.5 0-2.6 1.3-2.6 2.9 0 1.7 1.1 3 2.6 3s2.6-1.3 2.6-3c0-1.6-1.1-2.9-2.6-2.9z"
+      />
+    </svg>
+  );
+}
