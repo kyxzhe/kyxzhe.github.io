b45ff17d1b68941fc81cdece8345ebf810177c3f
KevinCarpricorn <zhengyuxiang8888@gmail.com>
2025-11-13 17:04:14 +1100

    优化导航玻璃效果并修复构建配置

---
+++ types/tailwind-merge.d.ts
@@ -0,0 +1,11 @@
+declare module 'tailwind-merge' {
+  export type ClassValue =
+    | string
+    | number
+    | null
+    | undefined
+    | ClassValue[]
+    | { [key: string]: boolean | string | number | undefined | null };
+
+  export function twMerge(...inputs: ClassValue[]): string;
+}
