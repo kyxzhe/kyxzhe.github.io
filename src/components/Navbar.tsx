8c3db6fcc6cfccdf7b3d233fd938c10c7078c10f
KevinCarpricorn <zhengyuxiang8888@gmail.com>
2025-11-21 23:50:29 +1100

    chore: stage navbar tweak

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,12 +15,12 @@ export default function Navbar() {
       >
         <Link
           href="/"
-          className="relative flex flex-row gap-2 items-center"
+          className="flex flex-row gap-2 items-center"
           aria-label="Go to homepage"
         >
           <span className="text-[1.26rem] md:text-[1.32rem] uppercase font-light italic tracking-[0.1em]">Kevin</span>
           <span className="text-[1.26rem] md:text-[1.32rem] uppercase font-semibold tracking-[0.1em]">Zheng</span>
-          <span className="absolute -right-3 -top-2 chip text-[0.55rem] uppercase tracking-[0.2em] px-[0.52rem] py-[0.32rem]">
+          <span className="chip text-[0.62rem] uppercase tracking-[0.21em] px-[0.6rem] py-[0.44rem]">
             Beta
           </span>
         </Link>
