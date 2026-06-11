026ea92e0adebd6586957f9b46de7b02a5dfb57e
KevinCarpricorn <zhengyuxiang8888@gmail.com>
2026-06-11 10:28:30 +1000

    chore(types): drop stale highlighter module

--- src/types/react-syntax-highlighter.d.ts
+++ src/types/react-syntax-highlighter.d.ts
@@ -1,4 +1,3 @@
-declare module "react-syntax-highlighter";
 declare module "react-syntax-highlighter/dist/esm/prism-light";
 declare module "react-syntax-highlighter/dist/esm/styles/prism/one-dark";
 declare module "react-syntax-highlighter/dist/esm/languages/prism/*";
